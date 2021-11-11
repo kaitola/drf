9a0da4fa36f1d6a9df1b0e4e996d8b57280edd0c
Matt Liu <shuyuliu105@gmail.com>
2021-11-11 09:58:11 +0000

    Modify application folder name

--- drf/urls.py
+++ drf/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from stock.views import index_view
+from app.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
